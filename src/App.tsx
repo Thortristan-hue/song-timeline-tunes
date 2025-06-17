6a85d20e7f9c2e7de9b10bb8a52d0f59ea87a1e5
Thortristan-hue <Thortristan@hotmail.com>
2025-06-18 01:48:26 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './styles/animations.css';
+import '@/styles/animations.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
